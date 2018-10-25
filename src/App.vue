14d15e8ae73e8dd8ef180607c0ec15f4dad97073
Victor Hugo Luna Resendiz <victorhugo.lunar@gmail.com>
2018-10-25 11:30:26 -0500

    Initial Import

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <!--<img src="./assets/logo.png">-->
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: white;
+    margin-top: 60px;
+  }
+  .bg-black {
+    background-color:#1a1a1a!important
+  }
+  a.bg-black:focus,
+  a.bg-black:hover,
+  button.bg-black:focus,
+  button.bg-black:hover {
+    background-color:#1d2124!important
+  }
+</style>
+
+<style src="./assets/vendor/fontawesome-free/css/all.min.css"></style>
+<style src="./assets/vendor/simple-line-icons/css/simple-line-icons.css"></style>
+<style src="./assets/css/landing-page.min.css"></style>
